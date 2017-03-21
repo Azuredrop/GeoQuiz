645af9dce7acbfcb53ad125c2abcc8058fb16f83
AzureDrop <taozhanbiao@gmail.com>
2017-03-21 18:04:23 +0800

    20170321_02 Chapter 1

--- app/src/main/java/azuredrop/geoquiz/QuizActivity.java
+++ app/src/main/java/azuredrop/geoquiz/QuizActivity.java
@@ -2,12 +2,34 @@ package azuredrop.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+    private Button mTrueBtn;
+    private Button mFalseBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueBtn = (Button)findViewById(R.id.true_btn);
+        mTrueBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseBtn = (Button)findViewById(R.id.false_btn);
+        mFalseBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
