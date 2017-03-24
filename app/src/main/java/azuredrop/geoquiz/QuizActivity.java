5aef51c15ae011ebd5e0268535214a1d39700d62
AzureDrop <taozhanbiao@gmail.com>
2017-03-24 10:47:43 +0800

    20170324_01 Chapter3

--- app/src/main/java/azuredrop/geoquiz/QuizActivity.java
+++ app/src/main/java/azuredrop/geoquiz/QuizActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+    private static final String KEY_INDEX = "index";
+
     private Button mTrueBtn;
     private Button mFalseBtn;
     private Button mNextBtn;
@@ -28,37 +30,73 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
         // TextView
-        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         updateQuestion();
 
         // TrueBtn
-        mTrueBtn = (Button)findViewById(R.id.true_btn);
-        mTrueBtn.setOnClickListener(new View.OnClickListener(){
+        mTrueBtn = (Button) findViewById(R.id.true_btn);
+        mTrueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 checkAnswer(true);
             }
         });
 
         // FalseBtn
-        mFalseBtn = (Button)findViewById(R.id.false_btn);
-        mFalseBtn.setOnClickListener(new View.OnClickListener(){
+        mFalseBtn = (Button) findViewById(R.id.false_btn);
+        mFalseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 checkAnswer(false);
             }
         });
 
         // NextBtn
-        mNextBtn = (Button)findViewById(R.id.next_btn);
-        mNextBtn.setOnClickListener(new View.OnClickListener(){
+        mNextBtn = (Button) findViewById(R.id.next_btn);
+        mNextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                mCurrentIndex = (mCurrentIndex+1)%mQuestionBank.length;
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 updateQuestion();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(KEY_INDEX, mCurrentIndex);
     }
 
     private void updateQuestion(){
